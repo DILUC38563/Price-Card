7fc30a4ffb43343db08d406279ff62a2f224b5e5
Aditya Bokade <85981154+DILUC38563@users.noreply.github.com>
2021-11-18 19:05:56 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,74 +1,63 @@
-import Button from '@material-ui/core/Button';
+
 import './App.css';
 
 function App() {
   return (
-  <div className = "main">
+    <div className="App">
+        <div className = "First">
+          <h2>Free</h2>
+          <h4 className = "more">$0<span className = "less">/month</span></h4>
+          <div className = "border"></div>
+          <div className = "content">
+          <p>✓ Single User</p>
+          <p>✓ 5GB Storage</p>
+          <p>✓ Unlimited Public Projects</p>
+          <p>✓ Community Access</p>
+          <p className = "light">✘ Unlimited Private Projects</p>
+          <p className = "light">✘ Dedicated Phone Support</p>
+          <p className = "light">✘ Free Subdomain</p>
+          <p className = "light">✘ Monthly Status Reports</p>
+          <button className = "button">Button</button>
+          </div>
+        </div>
 
-    <div className = "top">
 
-      <div className = "card-body">
-        <img src = "https://dummyimage.com/350x300/dee2e6/6c757d.jpg" alt = " a"></img>
-        <h3 className = "head">Fancy Product</h3>
-        <h4 className = "price">$ 40.00 - $ 80.00</h4>
-        <Button variant="outlined">View Option</Button>
-      </div>
-       
-      <div className = "card-body">
-        <img src = "https://dummyimage.com/350x300/dee2e6/6c757d.jpg" alt = " "></img>
-        <h3 className = "head">Special Item</h3>
-        <h4 className = "price"><s>$20.00</s> $18.00</h4>
-        <Button variant="outlined">Add to Cart</Button>
-      </div>
-        
-      <div className = "card-body">
-        <img src = "https://dummyimage.com/350x300/dee2e6/6c757d.jpg" alt = " "></img>
-        <h3 className = "head">Sale Item</h3>
-        <h4 className = "price"><s>$50.00</s> $25.00</h4>
-        <Button variant="outlined">Add to Cart</Button>
-      </div>
+        <div className = "First">
+          <h2>Plus</h2>
+          <h4 className = "more">$9<span className = "less">/month</span></h4>
+          <div className = "border"></div>
+          <div className = "content">
+          <p className = "bold">✓ 5 Users</p>
+          <p>✓ 50GB Storage</p>
+          <p>✓ Unlimited Public Projects</p>
+          <p>✓ Community Access</p>
+          <p>✓ Unlimited Private Projects</p>
+          <p>✓ Dedicated Phone Support</p>
+          <p>✓ Free Subdomain</p>
+          <p className = "light">✘ Monthly Status Reports</p>
+          <button className = "button">Button</button>
+          </div>
+        </div>  
 
-      <div className = "card-body">
-        <img src = "https://dummyimage.com/350x300/dee2e6/6c757d.jpg" alt = " "></img>
-        <h3 className = "head">Popular Item</h3>
-        <h4 className = "price">$ 40.00</h4>
-        <Button variant="outlined">Add to Cart</Button>
-      </div>
+
+        <div className = "First">
+          <h2>Pro</h2>
+          <h4 className = "more">$49<span className = "less">/month</span></h4>
+          <div className = "border"></div>
+          <div className = "content">
+          <p className = "bold">✓ Unlimited Users</p>
+          <p>✓ 150GB Storage</p>
+          <p>✓ Unlimited Public Projects</p>
+          <p>✓ Community Access</p>
+          <p>✓ Unlimited Private Projects</p>
+          <p>✓ Dedicated Phone Support</p>
+          <p>✓ Free Subdomain</p>
+          <p>✓ Monthly Status Reports</p>
+          <button className = "button">Button</button>
+          </div>
+        </div>  
 
     </div>
-
-    <div className = "bottom">
-
-      <div className = "card-body">
-        <img src = "https://dummyimage.com/350x300/dee2e6/6c757d.jpg" alt = " a"></img>
-        <h3 className = "head">Sale Item</h3>
-        <h4 className = "price"><s>$50.00</s> $25.00</h4>
-        <Button variant="outlined">View Option</Button>
-      </div>
-       
-      <div className = "card-body">
-        <img src = "https://dummyimage.com/350x300/dee2e6/6c757d.jpg" alt = " "></img>
-        <h3 className = "head">Fancy Product</h3>
-        <h4 className = "price">$120.00 - $280.00</h4>
-        <Button variant="outlined">View Option</Button>
-      </div>
-        
-      <div className = "card-body">
-        <img src = "https://dummyimage.com/350x300/dee2e6/6c757d.jpg" alt = " "></img>
-        <h3 className = "head">Special Item</h3>
-        <h4 className = "price"><s>$20.00</s> $18.00</h4>
-        <Button variant="outlined">Add to Cart</Button>
-      </div>
-
-      <div className = "card-body">
-        <img src = "https://dummyimage.com/350x300/dee2e6/6c757d.jpg" alt = " "></img>
-        <h3 className = "head">Popular Item</h3>
-        <h4 className = "price">$ 40.00</h4>
-        <Button variant="outlined">Add to Cart</Button>
-      </div>
-    </div>
-
-  </div>
   );
 }
 
